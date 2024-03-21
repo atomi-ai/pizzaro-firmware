351b809356c05c75489856b08911182a7c111bfb
Lv, Qi <lvsoft@gmail.com>
2024-03-22 04:03:19 +0800

    adjust motor150 parameters to increase maximum strength

--- pizzaro/src/bsp/config.rs
+++ pizzaro/src/bsp/config.rs
@@ -53,7 +53,7 @@ config! {
 parameters! {
     LINEAR_BULL: (f32, f32, f32)= (0.013, 0.0, 0.0), // ok
     // 左转最高速度，左转最小速度，右转最小速度，右转最高速度
-    HPD_BR_THRESHOLD: (f32, f32, f32, f32) = (0.03, 0.45, 0.55, 0.97),			   // ok
+    HPD_BR_THRESHOLD: (f32, f32, f32, f32) = (0.01, 0.45, 0.55, 0.99),			   // ok
     // 定位误差，pid和机械部分调的足够好之后，可以不需要这个误差值
     // HPD_POSITION_ERROR: u32 = 100,
     // for MC
@@ -65,6 +65,6 @@ parameters! {
     // PWM_TOP设置
     MMD_PERISTALTIC_PUMP_PWM_TOP: u16 = 5000,
     MMD_BRUSHLESS_MOTOR_PWM_TOP: u16 = 5000,
-    HPD_MOTOR150_PWM_TOP: u16 = 5000,
+    HPD_MOTOR150_PWM_TOP: u16 = 50000,
 
 }
