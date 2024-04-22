9749f4ca89366bd02302653fe759457bf034aba8
Zephyr Guo <zephyr@atomi.ai>
2024-04-21 21:34:20 -0700

    Enabled the CANbus along with some related modifications。

--- pizzaro/src/bin/hpd_main_v2.rs
+++ pizzaro/src/bin/hpd_main_v2.rs
@@ -11,52 +11,45 @@ use core::sync::atomic::Ordering;
 
 use can2040::{Can2040, CanFrame};
 use cortex_m::asm::delay;
-use defmt::{debug, Debug2Format, error, Format, info};
-use embedded_can::{Frame, Id, StandardId};
+use defmt::{debug, error, info, Debug2Format, Format};
 use embedded_can::nb::Can;
-use fugit::{ExtU64};
-use rp2040_hal::{
-    clocks::{init_clocks_and_plls},
-    pac,
-    sio::Sio,
-    Timer,
-    watchdog::Watchdog,
-};
+use embedded_can::{Frame, Id, StandardId};
+use fugit::ExtU64;
 use rp2040_hal::multicore::{Multicore, Stack};
+use rp2040_hal::{clocks::init_clocks_and_plls, pac, sio::Sio, watchdog::Watchdog, Timer};
 use rp_pico::{entry, XOSC_CRYSTAL_FREQ};
 use serde::Serialize;
 
 use generic::atomi_error::AtomiError;
-use generic::atomi_proto::{HpdCommand, LinearBullCommand, LinearBullResponse};
-use pizzaro::{hpd_sys_rx, hpd_sys_tx};
-use pizzaro::{hpd_br_nEN, hpd_br_pwm_a, hpd_br_pwm_b};
+use generic::atomi_proto::{AtomiErrorWithCanId, HpdCommand, LinearBullCommand, LinearBullResponse};
+use generic::mmd_status::MmdStatus;
 use pizzaro::bsp::config::{
     HPD_BR_DRIVER_N_EN, HPD_BR_THRESHOLD, HPD_MOTOR150_PWM_TOP, REVERT_HPD_BR_DIRECTION,
 };
 use pizzaro::common::async_initialization;
 use pizzaro::common::brush_motor::BrushMotor;
-use pizzaro::common::consts::{CANBUS_FREQUENCY, HPD_CAN_ID, MC_CAN_ID};
+use pizzaro::common::can::{
+    enqueue_can_frame_if_possible, get_can, init_can_bus, send_can_message,
+};
+use pizzaro::common::consts::{CANBUS_FREQUENCY, HPD_CAN_ID, MC_CAN_ID, MMD_CAN_ID};
 use pizzaro::common::executor::{spawn_task, start_global_executor};
-use pizzaro::common::global_timer::{Delay, init_global_timer};
+use pizzaro::common::global_timer::{init_global_timer, Delay};
 use pizzaro::common::message_queue::{MessageQueueInterface, MessageQueueWrapper};
 use pizzaro::common::once::Once;
 use pizzaro::common::rp2040_timer::Rp2040Timer;
-use pizzaro::hpd::GLOBAL_LINEAR_BULL_STOP;
 use pizzaro::hpd::hpd_misc::LinearScale;
 use pizzaro::hpd::linear_bull_processor::{
-    linear_bull_input_mq, linear_bull_output_mq, LinearBullProcessor, process_linear_bull_message,
+    linear_bull_input_mq, linear_bull_output_mq, process_linear_bull_message, LinearBullProcessor,
 };
 use pizzaro::hpd::linear_scale::{core1_task, read_and_update_linear_scale};
+use pizzaro::hpd::GLOBAL_LINEAR_BULL_STOP;
+use pizzaro::{hpd_br_nEN, hpd_br_pwm_a, hpd_br_pwm_b};
+use pizzaro::{hpd_sys_rx, hpd_sys_tx};
 
 struct GlobalContainer {
     linear_scale: Option<LinearScale>,
 }
 static mut GLOBAL_CONTAINER: GlobalContainer = GlobalContainer { linear_scale: None };
-
-static mut CAN_BUS_ONCE: Once<Can2040> = Once::new();
-fn get_can() -> &'static mut Can2040 {
-    unsafe { CAN_BUS_ONCE.get_mut_or_fail() }
-}
 
 static mut HPD_MESSAGE_QUEUE_ONCE: Once<MessageQueueWrapper<HpdCommand>> = Once::new();
 fn get_hpd_mq() -> &'static mut MessageQueueWrapper<HpdCommand> {
@@ -132,16 +125,12 @@ fn main() -> ! {
 
     {
         // init CAN bus here.
-        unsafe {
-            CAN_BUS_ONCE.init_once_with_function(|| {
-                can2040::initialize_cbus(
-                    &mut core,
-                    CANBUS_FREQUENCY,
-                    hpd_sys_rx!(pins).id().num as u32,
-                    hpd_sys_tx!(pins).id().num as u32,
-                )
-            });
-        }
+        init_can_bus(
+            &mut core,
+            CANBUS_FREQUENCY,
+            hpd_sys_rx!(pins).id().num as u32,
+            hpd_sys_tx!(pins).id().num as u32,
+        );
         spawn_task(process_can_hpd_message());
     }
 
@@ -150,66 +139,6 @@ fn main() -> ! {
     loop {
         info!("in loop");
         delay(120_000_000);
-    }
-}
-
-fn possibly_enqueue_hpd_message(f: CanFrame) {
-    debug!("frame_to_hpd_msg() : convert can msg: {}", f);
-    // 确认是HPD的消息。
-    match f.id() {
-        Id::Standard(sid) => {
-            if sid.as_raw() != HPD_CAN_ID {
-                // ignore HPD unrelated msgs.
-                return;
-            }
-        }
-        _ => {
-            // Unsupport ids.
-            return;
-        }
-    }
-    match postcard::from_bytes::<HpdCommand>(&f.data()[..f.dlc()]) {
-        Ok(hpd_msg) => get_hpd_mq().enqueue(hpd_msg),
-        Err(err) => {
-            error!("frame_to_hpd_msg(): errors in parsing HPD message, err: {}", Debug2Format(&err))
-        }
-    }
-}
-
-async fn process_can_hpd_message() {
-    loop {
-        Delay::new(1.millis()).await;
-
-        match get_can().receive() {
-            Ok(f) => {
-
-                possibly_enqueue_hpd_message(f);
-            }
-            Err(nb::Error::Other(err)) => {
-                error!("Canbus error: {}", err);
-            }
-            Err(_) => (),
-        }
-    }
-}
-
-// TODO(zephyr): Move the function to common/
-fn send_can_message<U: Format + Serialize>(cob_id: u16, msg: U) -> Result<(), AtomiError> {
-    info!("send_can_message(): to transmit msg: {}", msg);
-    let data = postcard::to_allocvec::<U>(&msg).map_err(|_| AtomiError::UartInvalidInput)?;
-    let f =
-        CanFrame::new(StandardId::new(cob_id).ok_or(AtomiError::CanCobIdError)?, data.as_slice())
-            .ok_or(AtomiError::CanFrameError)?;
-    debug!("send_can_message(): to transmit frame: {}", f);
-    match <Can2040 as embedded_can::blocking::Can>::transmit(get_can(), &f) {
-        Ok(_) => {
-            info!("Transmitted package: {:?}", f);
-            Ok(())
-        }
-        Err(err) => {
-            error!("Transmit error: {}", err);
-            Err(AtomiError::CanTransmitError)?
-        }
     }
 }
 
@@ -222,9 +151,7 @@ async fn hpd_process_messages() {
             info!("[HPD] process_messages() 1.1 | dequeued message: {}", message);
             // 处理消息
             let res = match message {
-                HpdCommand::HpdPing => {
-                    send_can_message(MC_CAN_ID, HpdCommand::HpdPong)
-                }
+                HpdCommand::HpdPing => send_can_message(MC_CAN_ID, HpdCommand::HpdPong),
 
                 HpdCommand::HpdStop => {
                     if !linear_bull_available {
@@ -246,10 +173,9 @@ async fn hpd_process_messages() {
                     if linear_bull_available {
                         send_can_message(MC_CAN_ID, HpdCommand::HpdAck)
                     } else {
-                        // TODO(zephyr): 逻辑变了，MC端要一起改下，注释掉的代码如下：
-                        // let _ = uart_comm.send(AtomiProto::AtomiError(AtomiError::HpdUnavailable));
-                        // Err(AtomiError::HpdUnavailable)
-                        send_can_message(MC_CAN_ID, HpdCommand::HpdBusy)
+                        let _ = send_can_message(MC_CAN_ID, AtomiErrorWithCanId::new(
+                            HPD_CAN_ID, AtomiError::HpdUnavailable));
+                        Err(AtomiError::HpdUnavailable)
                     }
                 }
 
@@ -260,10 +186,9 @@ async fn hpd_process_messages() {
                         linear_bull_available = false;
                         res
                     } else {
-                        // TODO(zephyr): 逻辑变了，MC端要一起改下，注释掉的代码如下：
-                        // let _ = uart_comm.send(AtomiProto::AtomiError(AtomiError::HpdUnavailable));
-                        // Err(AtomiError::HpdUnavailable)
-                        send_can_message(MC_CAN_ID, HpdCommand::HpdBusy)
+                        let _ = send_can_message(MC_CAN_ID, AtomiErrorWithCanId::new(
+                            HPD_CAN_ID, AtomiError::HpdUnavailable));
+                        Err(AtomiError::HpdUnavailable)
                     }
                 }
                 _ => Err(AtomiError::IgnoredMsg), // Ignore unrelated commands
@@ -276,6 +201,22 @@ async fn hpd_process_messages() {
         if let Some(linear_bull_resp) = linear_bull_output_mq().dequeue() {
             info!("[HPD] get response from linear bull: {}", linear_bull_resp);
             linear_bull_available = true;
+        }
+    }
+}
+
+async fn process_can_hpd_message() {
+    loop {
+        Delay::new(100.micros()).await;
+
+        match get_can().receive() {
+            Ok(f) => {
+                enqueue_can_frame_if_possible(HPD_CAN_ID, f, get_hpd_mq());
+            }
+            Err(nb::Error::Other(err)) => {
+                error!("Canbus error: {}", err);
+            }
+            Err(_) => (),
         }
     }
 }
