bd9cbeadc4a123b157398a5ee7b687ec947d3730
Lv, Qi <lvsoft@gmail.com>
2024-04-24 19:01:41 +0800

    update run script for new defmt log level syntax

--- script/run_dtu.sh
+++ script/run_dtu.sh
@@ -1,3 +1,3 @@
-DEFMT_LOG=info cargo run --bin dtu_main --release --config 'target."cfg(all(target_arch = \"arm\", target_os = \"none\"))".runner="probe-rs run --probe 2e8a:4005:E6635C469F10842D --chip RP2040 --protocol swd --speed 1000"'
+DEFMT_LOG=pizzaro=debug,can2040=debug,info cargo run --bin dtu_main --release --config 'target."cfg(all(target_arch = \"arm\", target_os = \"none\"))".runner="probe-rs run --probe 2e8a:4005:E6635C469F10842D --chip RP2040 --protocol swd --speed 1000"'
 
 #cargo run --bin dtu_main --release --config 'target."cfg(all(target_arch = \"arm\", target_os = \"none\"))".runner="probe-rs run --probe 2e8a:4005:E6635C469F10842D --chip RP2040 --protocol swd --speed 1000"'
--- script/run_hpd.sh
+++ script/run_hpd.sh
@@ -1,2 +1,2 @@
 # pizzaro device No.1
-DEFMT_LOG=info cargo run --release --bin hpd_main --config 'target."cfg(all(target_arch = \"arm\", target_os = \"none\"))".runner="probe-rs run --probe 2e8a:4005:E6635C469F2A432E --chip RP2040 --protocol swd --speed 1000"'
+DEFMT_LOG=pizzaro=info,can2040=debug,info cargo run --release --bin hpd_main --config 'target."cfg(all(target_arch = \"arm\", target_os = \"none\"))".runner="probe-rs run --probe 2e8a:4005:E6635C469F2A432E --chip RP2040 --protocol swd --speed 1000"'
